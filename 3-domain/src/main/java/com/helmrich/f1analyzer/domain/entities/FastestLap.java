6a37d49f4f5d70cb52d08cae24a97374afd1a3f9
Ian Helmrich <ian.helmrich@gmail.com>
2022-06-04 16:44:48 +0200

    add gitignore

--- 3-domain/src/main/java/com/helmrich/f1analyzer/domain/entities/FastestLap.java
+++
@@ -1,6 +0,0 @@
-package com.helmrich.f1analyzer.domain.entities;
-
-public class FastestLap {
-    private String time;
-    private String averageSpeed;
-}
--- 3-domain/src/main/java/com/helmrich/f1analyzer/domain/enums/Status.java
+++
@@ -1,7 +0,0 @@
-package com.helmrich.f1analyzer.domain.enums;
-
-public enum Status {
-    FINISHED,
-    ENGINE,
-    STEERING
-}
