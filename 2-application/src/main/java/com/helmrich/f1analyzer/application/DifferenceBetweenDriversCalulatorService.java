5ba5b7c27f403e2fae6a9917b2fd4a6715fc8ac3
Ian Helmrich <ian.helmrich@gmail.com>
2022-06-04 22:38:43 +0200

    delete repeating code

--- 2-application/src/main/java/com/helmrich/f1analyzer/application/DifferenceBetweenDriversCalulatorService.java
+++ 2-application/src/main/java/com/helmrich/f1analyzer/application/DifferenceBetweenDriversCalulatorService.java
@@ -4,21 +4,22 @@ import com.helmrich.f1analyzer.domain.entities.RaceResult;
 import com.helmrich.f1analyzer.domain.entities.Result;
 import com.helmrich.f1analyzer.domain.interfaces.ConsoleLoggingInterface;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class DifferenceBetweenDriversCalulatorService {
     private final ConsoleLoggingInterface consoleLogger;
+    private Result firstResult = null;
+    private Result secondResult = null;
 
     public DifferenceBetweenDriversCalulatorService(ConsoleLoggingInterface consoleLogger) {
         this.consoleLogger = consoleLogger;
     }
 
-    public void getTimeDifference(String firstDriverCode, String secondDriverCode, RaceResult raceResult) {
+    private List<Result> getDriverCodes(String firstDriverCode, String secondDriverCode, RaceResult raceResult) {
         List<Result> results = raceResult.getResultList();
-        Result firstResult = null;
-        Result secondResult = null;
         for (int i = 0; i < results.size(); i++) {
             String driverCode = results.get(i).getDriver().getCode();
             if (Objects.equals(driverCode, firstDriverCode)) {
@@ -27,6 +28,16 @@ public class DifferenceBetweenDriversCalulatorService {
                 secondResult = results.get(i);
             }
         }
+        List<Result> driverResults = new ArrayList<>();
+        driverResults.add(firstResult);
+        driverResults.add(secondResult);
+        return driverResults;
+    }
+
+    public void getTimeDifference(String firstDriverCode, String secondDriverCode, RaceResult raceResult) {
+        List<Result> driverResults = getDriverCodes(firstDriverCode, secondDriverCode, raceResult);
+        firstResult = driverResults.get(0);
+        secondResult = driverResults.get(1);
         if (firstResult == null) consoleLogger.log("Driver Code of first driver is wrong");
         if (secondResult == null) consoleLogger.log("Driver Code of second driver is wrong");
         else {
@@ -38,17 +49,9 @@ public class DifferenceBetweenDriversCalulatorService {
     }
 
     public void getSpeedDifference(String firstDriverCode, String secondDriverCode, RaceResult raceResult) {
-        List<Result> results = raceResult.getResultList();
-        Result firstResult = null;
-        Result secondResult = null;
-        for (int i = 0; i < results.size(); i++) {
-            String driverCode = results.get(i).getDriver().getCode();
-            if (Objects.equals(driverCode, firstDriverCode)) {
-                firstResult = results.get(i);
-            } else if (Objects.equals(driverCode, secondDriverCode)) {
-                secondResult = results.get(i);
-            }
-        }
+        List<Result> driverResults = getDriverCodes(firstDriverCode, secondDriverCode, raceResult);
+        firstResult = driverResults.get(0);
+        secondResult = driverResults.get(1);
         if (firstResult == null) consoleLogger.log("Driver Code of first driver is wrong");
         if (secondResult == null) consoleLogger.log("Driver Code of second driver is wrong");
         else {
